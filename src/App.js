0284abccaf565eb2122f91208fe3e5981b2b1b79
Frankincense <frankincensewelsey@gmail.com>
2025-04-18 09:52:16 +0300

    testing events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 function App() {
+  const [pepperoniChecked, setChecked] = useState(false)
+
+  function handleClick(){
+    setChecked(!pepperoniChecked)
+  }
   return (
-    <div></div>
+    <div>
+      <input 
+      type="checkbox" 
+      name="addPepperoni" 
+      id="addPepperoni" 
+      onChange={handleClick}
+      aria-checked={pepperoniChecked} 
+      checked={pepperoniChecked}/>
+      <label htmlFor="addPepperoni">add pepperoni</label>
+
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoniChecked ? 
+        <li>Pepperoni</li>:
+         null}
+      </ul>
+    </div>
   )
 }
 
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,5 +2,48 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
+import { toBeChecked } from "@testing-library/jest-dom/dist/matchers";
 
 // Code tests here
+// Test that the initial page load is as expected.
+test("pizza checkbox is initially unchecked", () => {
+    render(<App />);
+
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+    expect(addPepperoni).not.toBeChecked();
+})
+
+test("toppings list initially contains only cheese", () => {
+    render(<App />);
+
+    expect(screen.getAllByRole("listitem").length).toBe(1);
+    expect(screen.getByText("Cheese")).toBeInTheDocument();
+    expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument();
+})
+// Test that the checkbox triggers and event
+test("checkboxes become checked when user clicks them", () => {
+    render(<App />);
+
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+    userEvent.click(addPepperoni)
+
+    expect(addPepperoni).toBeChecked();
+
+});
+
+test("topping appears in toppings list when checked", () => {
+    render(<App />);
+
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+    userEvent.click(addPepperoni);
+
+    expect(screen.getAllByRole("listitem").length).toBe(2);
+    expect(screen.getByText("Cheese")).toBeInTheDocument();
+    expect(screen.getByText("Pepperoni")).toBeInTheDocument();
+});
+
+// Test the effect of clicking the check box a second time
